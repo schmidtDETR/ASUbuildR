c6af0e19ca4da59e749b4429482d9d6cf7deee33
David Schmidt <deschmidt@detr.nv>
2025-07-17 09:04:05 -0700

    Initial commit

---
+++ R/launch_ASUbuildR.R
@@ -0,0 +1,53 @@
+# Script to run ASU Flexdashboard RMD file
+# This script renders and launches the Shiny-based flexdashboard
+
+# Load required libraries
+library(rmarkdown)
+library(flexdashboard)
+library(shiny)
+
+#' launch_ASUbuildR
+#'
+#' This function renders and launches a Shiny-based R Markdown Flexdashboard.
+#' The dashboard will automatically open in the default web browser.
+#'
+#' @param rmd_file Character string specifying the path to the R Markdown file.
+#'   Default is "ASU Flexdashboard.Rmd".
+#'
+#' @return This function is called for its side effects (launching the dashboard).
+#'   It does not return a value.
+#'
+#' @examples
+#' \dontrun{
+#' # Run dashboard with default file name
+#' run_asu_dashboard()
+#'
+#' # Run dashboard with custom file name
+#' launch_ASUbuildR("MyCustomDashboard.Rmd")
+#' }
+#'
+#' @seealso \code{\link[rmarkdown]{run}} for more details on running Shiny documents
+#'
+#' @export
+launch_ASUbuildR <- function(rmd_file = "R/ASU Flexdashboard.Rmd") {
+
+  # Check if the RMD file exists
+  if (!file.exists(rmd_file)) {
+    stop(paste("File", rmd_file, "not found in current directory"))
+  }
+
+  # Print status message
+  cat("Starting ASU Flexdashboard...\n")
+  cat("File:", rmd_file, "\n")
+
+  # Run the flexdashboard with shiny
+  # This will render and launch the dashboard in the browser
+  rmarkdown::run(
+    file = rmd_file,
+    shiny_args = list(
+      host = "127.0.0.1",  # localhost
+      port = NULL,         # Let R choose an available port
+      launch.browser = TRUE # Automatically open in browser
+    )
+  )
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# ASUbuildR
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+ASUbuildR provides a GUI interface to equip state to more easily
+generate Areas of Substantial Unemployment (ASU) that maximize
+unemploymnet within ASUs in a state. This package depends on receiving
+an Excel file from the U.S. Bureau of Labor Statistics which includes
+their tract-level labor force estimates reflecting the twelve months
+ending in June, with preliminary estimates for June, revised estimates
+for January-May, and benchmarked estimates for July-December in the
+prior year.
+
+## Installation
+
+You can install the development version of ASUbuildR from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("pak")
+pak::pak("schmidtDETR/ASUbuildR")
+```
+
+## Running the Application
+
+ASUbuildR launches an interactive Shiny application, which you can open
+by running this function:
+
+``` r
+#ASUbuildR::launch_ASUbuildR()
+```
